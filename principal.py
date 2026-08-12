09f12a53367760a17b3e9ac97ccc06799270999a
Vinicius Machado <viniciuscarneiromachado@gmail.com>
2026-08-12 19:58:06 -0300

    Add files via upload

--- principal.py
+++ principal.py
@@ -12,9 +12,29 @@ class Game:
         #ciando a tela do jogo
         self.tela = pygame.display.set_mode((constantes.LARGURA, constantes.ALTURA))
         #exibindo o título do jogo
-        pygamre.display.set_caption(constantes.TITULO_JOGO)
+        pygame.display.set_caption(constantes.TITULO_JOGO)
         #criando o relógio do jogo
         self.relogio = pygame.time.Clock()
         self.jogando = True
         self.fonte = pygame.font.SysFont('Arial', 24)
         self carregar_arquivos()
+        def novo_jogo(self):
+            #instanciar as imagens
+            self.todas_as_imagens = pygame.sprite.Group()
+            self.rodar()   
+        def rodar(self)
+            #loop principal do jogo
+            self.jogando = True
+            while self.jogando:
+                self.relogio.tick(constantes.FPS)
+                self.eventos()
+                self.atualizar_sprites()
+                self.desenhar_sprites()
+
+            def eventos(self):
+                #loop de eventos de jogo
+                for evento in pygame.event.get():
+                    if evento.type == pygame.QUIT:
+                        if self.jogando:
+                            self.jogando = False
+                            self.esta_rodando = False
